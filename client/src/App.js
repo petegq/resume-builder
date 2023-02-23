c8698c9671e99d41fe4c1534740f9d935fbc3b98
Peter Garside-Quin <petegq@gmail.com>
2023-02-23 18:34:38 +0000

    Format

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,19 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./components/Home";
-import Resume from "./components/Resume";
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './components/Home'
+import Resume from './components/Resume'
 
 const App = () => {
-    return (
-        <div>
-            <BrowserRouter>
-                <Routes>
-                    <Route path='/' element={<Home />} />
-                    <Route path='/resume' element={<Resume />} />
-                </Routes>
-            </BrowserRouter>
-        </div>
-    );
-};
+	return (
+		<div>
+			<BrowserRouter>
+				<Routes>
+					<Route path='/' element={<Home />} />
+					<Route path='/resume' element={<Resume />} />
+				</Routes>
+			</BrowserRouter>
+		</div>
+	)
+}
 
-export default App;
+export default App
